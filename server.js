61ebfa87dfe1830da6108b8a58d316a1f9be90aa
Tarik J <tarik.jaidani@gmail.com>
2024-09-12 01:04:42 +0200

    add Delete

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/blog', (req,res) => {
     res.send('Hello blog')
 })
 
-
+// get all products
 
 app.get('/products',  async(req,res) => {
     try {
@@ -31,6 +31,7 @@ app.get('/products',  async(req,res) => {
     }
 })
 
+// get a specific product
 
 app.get('/products/:id',  async(req,res) => {
     try {
@@ -43,6 +44,7 @@ app.get('/products/:id',  async(req,res) => {
     }
 })
 
+// add a specific product
 app.post('/products', async (req,res) => {
   try {
     const product = await Product.create(req.body)
@@ -55,11 +57,10 @@ app.post('/products', async (req,res) => {
 })
 
 
-/// update
+/// update a specific product
 
 app.put('/products/:id', async (req,res) => {
     try {
-
         const {id} = req.params
       const product = await Product.findByIdAndUpdate(id, req.body)
         // No product founded
@@ -69,14 +70,33 @@ app.put('/products/:id', async (req,res) => {
             const updatedProduct = await Product.findById(id)
             res.status(200).json(updatedProduct)
         }
-      
-      
+            
     } catch (error) {
       console.log(error.message)
       res.status(500).json({message: error.message})
     }
   })
 
+// Delete a product 
+
+app.delete("/products/:id", async (req,res)=>{
+      try {
+        const {id} = req.params
+        const product = await Product.findByIdAndDelete(id)
+        // No product founded
+        if(!product) {
+            return res.status(404).json({message: `Cannot find the id: ${id}`})
+        } 
+        res.status(200).json(product)
+        console.log("Product deleted")
+        
+      } catch (error) {
+        res.status(500).json({message: error.message})
+      }
+})
+
+
+// Database connection
 mongoose.connect('mongodb+srv://tarikjaidani:123456*@cluster0.ppcyl.mongodb.net/API-Node?retryWrites=true&w=majority&appName=Cluster0/')
 .then(()=>{
 
