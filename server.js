bbe9eaa80eff7bf943b048011befd9758ad81299
Tarik J <tarik.jaidani@gmail.com>
2024-09-13 00:03:38 +0200

    Cors

--- server.js
+++ server.js
@@ -4,16 +4,27 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const errorMiddleware = require('./middleware/errorMiddleware')
-
-
-app.use(express.json())
+const cors = require('cors')
 
 const port =process.env.port
 const MONGO_URL = process.env.MONGO_URL
+const FRONTEND = process.env.FRONTEND
+
+
+var corsOptions = {
+    origin: FRONTEND,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
 
 app.use(express.json())
 // middelware setup
 app.use(express.urlencoded({extended: false}))
+
+
 
 // Routes
 app.use('/api/products', productRoute)
